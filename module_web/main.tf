320acb82578fad1f5abc9eabfaeb70b1c410da75
orpere <orlando.capoeiraraiz@gmail.com>
2019-11-08 10:57:27 +0100

    fix

--- module_web/main.tf
+++ module_web/main.tf
@@ -1,20 +1,6 @@
-resource "aws_instance" "web" {
-  count = "${var.instance_count}"
-  # count initiate the default variable count as value 2 
+resource "null_resource" "outputsStudy" {
+  provisioner "local-exec" {
+    command = "echo this is the  ${count.index} outputsStudy"
+  }
+  count = 2
 }
-
-resource "null_resource" "web" {
-  ami           = "${var.ami_id}"
-  instance_type = "${var.instance_type}"
-  connection {
-    host = "${aws_instance.web.public_ip}"
-  }
-
-  tags = {
-    Name = "web ${var.instance_count.index+1}/${var.instance_count}"
-  }
-   provisioner "local-exec" {
-    command = "echo ${aws_instance.web.public_ip} >> my_infrastructure.txt" # this will add the ip to the terrafom local machine
-  }
-}
-
--- module_web/outputs.tf
+++ module_web/outputs.tf
@@ -1,3 +1,3 @@
-output "public_ip" {
-  value = "${null_resource.web.*.public_ip}"
+output "id" {
+  value = "${null_resource.outputsStudy}"
 }
